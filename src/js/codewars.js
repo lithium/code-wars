d119d0569ea3c1fa1a85e9c154d54c634eb1dbdc
lithium <code@hlidskialf.com>
2013-11-07 11:12:12 -0800

    basic editor with tab indent and smart backspace

--- src/js/codewars.js
+++ src/js/codewars.js
@@ -1,3 +1,79 @@
+
+
+
+AsmEditor = Backbone.View.extend({
+  el: $('<textarea class="editor"></textarea>'),
+
+  events: {
+    'keydown ': "keyDown",
+    'keyup': "keyUp",
+    'change ': "change",
+  },
+
+  initialize: function() {
+
+    this.contents = []
+
+  },
+
+  render: function() {
+    this.$el.val(this.contents.join(''))
+  },
+
+
+  keyDown: function(evt) {
+    if (evt.keyCode == 9) {
+      return this.performTab(evt);
+    }
+    else if (evt.keyCode == 8) {
+      return this.performBackspace(evt);
+    }
+    // console.log("code", evt.keyCode);
+
+    return true;
+  },
+  keyUp: function(evt) {
+    this.updateContents();
+    return true;
+  },
+  change: function() {
+    this.updateContents();
+  },
+  updateContents: function() {
+    this.contents = this.$el.val().split('');
+  }
+
+
+  performTab: function() {
+
+    var cursorPosition = this.$el.prop("selectionStart");
+    this.contents.splice(cursorPosition, 0, ' ',' ',' ',' ');
+
+    this.render();
+
+    return false;
+  },
+
+  performBackspace: function() {
+    var cursorPosition = this.$el.prop("selectionStart");
+
+    var chars = this.contents.slice(cursorPosition-4,cursorPosition).join('')
+    if (chars == "    ") {
+      this.contents.splice(cursorPosition-4,4)
+      this.render();
+      return false;
+    }
+
+    return true;
+  },
+
+  performReturn: function() {
+
+  },
+
+});
+
+
 CodeWarsConsole = Backbone.View.extend({
   el: $('#console.codewars'),
 
@@ -7,8 +83,10 @@ CodeWarsConsole = Backbone.View.extend({
 
   initialize: function() {
 
-    this.editor = this.$(".editor.asm");
+    this.editor = new AsmEditor({el: this.$(".editor")});
+    this.output = this.$(".output.compiled");
 
+    this.editor.el.focus();
   },
 
   render: function() {
@@ -16,8 +94,12 @@ CodeWarsConsole = Backbone.View.extend({
   },
 
   saveEditor: function() {
-    console.log(this.editor.val())
+    var playerScript = this.editor.val();
+    var compiledScript = RedAsm.compile(playerScript);
+
+    this.output.html(compiledScript);
 
   },
+
 
 })
