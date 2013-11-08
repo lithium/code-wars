e64cb4dbaa429b31d588cf57aa2b8cc8dddb6e14
lithium <code@hlidskialf.com>
2013-11-07 19:46:38 -0800

    help well and query ui

--- src/js/codewars.js
+++ src/js/codewars.js
@@ -10,16 +10,60 @@ CodeWarsConsole = Backbone.View.extend({
   initialize: function() {
     this.output = this.$(".output.compiled");
 
+
+    this.firstColumn = this.$(".firstColumn")
+    this.secondColumn = this.$(".secondColumn")
+    this.thirdColumn = this.$(".thirdColumn")
+    this.hideThirdColumn();
+
+    this.help = this.$(".help.well");
+
     this.errors = this.$(".errors");
 
     this.editor = ace.edit(this.$(".editor")[0]);
     this.editor.setTheme("ace/theme/github");
     this.editor.setOption("firstLineNumber", 0);
+
+    this.editor.selection.on("changeCursor", _.bind(this.editorCursorChanged, this));
     this.editor.focus();
   },
 
   render: function() {
 
+  },
+
+  hideThirdColumn: function() {
+    this.thirdColumnShown = false;
+
+    var duration = 0;
+    this.firstColumn.switchClass("col-md-5", "col-md-7", duration);
+    this.secondColumn.switchClass("col-md-4", "col-md-5", duration);
+    this.thirdColumn.switchClass("col-md-3", "col-md-0", duration);
+
+  },
+  showThirdColumn: function() {
+    if (this.thirdColumnShown)
+      return;
+
+    var duration = 0;
+    this.firstColumn.switchClass("col-md-7", "col-md-5", duration);
+    this.secondColumn.switchClass("col-md-5", "col-md-4", duration);
+    this.thirdColumn.switchClass("col-md-0", "col-md-3", duration);
+
+    this.thirdColumnShown = true;
+  },
+
+  editorCursorChanged: function(evt, selection) {
+      var cursor = selection.getCursor()
+      var wordRange = this.editor.session.getWordRange(cursor.row, cursor.column);
+      var line = this.editor.session.getLine(cursor.row)
+      var word = line.substring(wordRange.start.column, wordRange.end.column)
+      if (word.toUpperCase() in RedAsm.MNEUMONICS) {
+        this.showThirdColumn();
+        this.help.html(word);
+      } else {
+        this.hideThirdColumn();
+      }
   },
 
   saveEditor: function() {
