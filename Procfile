9099d78e78e047d44e2001dece859d46667026b9
lithium <code@hlidskialf.com>
2013-11-09 12:26:25 -0800

    heroku nodejs skeleton

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node referee/server.js
---
+++ referee/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var port = process.env.PORT || 5000;
+
+var app = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello User\n');
+});
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
