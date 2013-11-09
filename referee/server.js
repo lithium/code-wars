a764a8965fc6534b3bc3c700afc86a31f2915efb
lithium <code@hlidskialf.com>
2013-11-09 13:48:36 -0800

    expressjs and redis

--- referee/server.js
+++ referee/server.js
@@ -1,11 +1,43 @@
-var http = require('http');
-var port = process.env.PORT || 5000;
+/*
+ * Code Wars Referee
+ *  node.js, express, redis
+ *
+ */
 
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('> Hello, User.\n');
+// imports
+var express = require('express')
+var redis_url = require('redis-url');
+
+
+// environment from heroku
+var port = process.env.PORT || 5000;
+var redistogo_url = process.env.REDISTOGO_URL;
+
+
+
+// application objects
+var app = express();
+var redis = redis_url.connect(redistogo_url)
+
+
+
+
+// routes
+app.get('/', function(req, res) {
+  redis.get('foo', function(e,foo) {
+    res.send({'message': "Hello, "+foo});
+  });
 });
 
+
+app.get('/foo', function(req, res) {
+  // redis.set('foo', req);
+  var foo = req.connection.remoteAddress;
+  redis.set("foo", foo);
+  res.send(foo);
+});
+
+// main loop
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
