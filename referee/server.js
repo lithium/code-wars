ea4ee3a2dd01c5f9614c384fd432470ba4d1f933
lithium <code@hlidskialf.com>
2013-11-09 12:46:49 -0800

    referee change

--- referee/server.js
+++ referee/server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 5000;
 
 var app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello User\n');
+  res.end('> Hello, User.\n');
 });
 
 app.listen(port, function() {
